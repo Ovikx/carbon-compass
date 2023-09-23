84cb5ba9190272f5f11cecd9234cedb4cea5e879
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2023-09-23 17:21:33 -0500

    Did leaderboard

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -1,32 +1,59 @@
+import { useState, useEffect } from "react";
+import { getUsersFromLeaderboard } from "../firebase/controller";
+import { User } from "../model/User";
+import ClipLoader from "react-spinners/ClipLoader";
+
 export function Leaderboard() {
   // leaderboard page for users to see how they compare to others
-
-  return (
-    <div className="flex flex-col">
+  // getUsersFromLeaderboard("group 1")
+  const [users, updateUsers] = useState<User[] | null>();
+  useEffect(() => {
+    const getData = async () => {
+      const userList = [new User("Ovik", "", 150), new User("James", "", 10)]; //await getUsersFromLeaderboard("group 1");
+      updateUsers(userList);
+    };
+    getData();
+  }, []);
+  if (!users) {
+    return (
       <div>
-        <table className="table-auto">
+        <h1>Loading...</h1>
+        <ClipLoader color={"#ffffff"} loading={!users} size={150} />
+      </div>
+    );
+  }
+  return (
+    users && (
+      <div className="flex flex-col">
+        <div>
+          <h1 className="text-4xl text-center text-slate-700 dark:text-slate-400 font-bold mt-10 mb-3">
+            Leaderboard
+          </h1>
+          <h2 className="text-2xl text-center text-slate-700 dark:text-slate-400 font-bold mb-5">
+            Group 1
+          </h2>
+        </div>
+        <table className="table-auto m-auto text-lg border-none">
           <thead>
             <tr>
-              <th className="px-4 py-2">Name</th>
-              <th className="px-4 py-2">Carbon Offset</th>
+              <th className="px-4 py-2 text-slate-700">Rank</th>
+              <th className="px-4 py-2 text-slate-700">Name</th>
+              <th className="px-4 py-2 text-slate-700">Carbon Saved</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className="border px-4 py-2">John</td>
-              <td className="border px-4 py-2">10</td>
-            </tr>
-            <tr className="bg-gray-100">
-              <td className="border px-4 py-2">Jane</td>
-              <td className="border px-4 py-2">5</td>
-            </tr>
-            <tr>
-              <td className="border px-4 py-2">Bob</td>
-              <td className="border px-4 py-2">7</td>
-            </tr>
+            {users.map((user: User, index: number) => {
+              return (
+                <tr className="border-b border-slate-400 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                  <td className="px-4 py-2">{index + 1}</td>
+                  <td className="px-4 py-2">{user.id}</td>
+                  <td className="bpx-4 py-2">{user.carbonSaved}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
-    </div>
+    )
   );
 }
