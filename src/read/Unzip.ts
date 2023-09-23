cbb73f247d953ae2825ec51c95aaab38f28dc544
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2023-09-23 11:40:15 -0500

    Add zip.js and parsing through zip file

---
+++ src/read/Unzip.ts
@@ -0,0 +1,43 @@
+// unzip Location History zip archive from Google Takeout
+
+import * as zip from "@zip.js/zip.js";
+import { Deserialize } from "./Deserialize.ts";
+import { Location } from "../model/Location.ts";
+import { Route } from "../model/Route.ts";
+import { Activity } from "../model/Activity.ts";
+
+export class Unzip {
+    public static async unzipLocationHistory(blob: Blob): Promise<void> {
+        // unzip using zip.js using filereader
+        const reader = new zip.ZipReader(new zip.BlobReader(blob));
+        const entries = await reader.getEntries();
+        const locationPath = entries.find((entry) => entry.filename === "Takeout/Location History/Records.json");
+        const possibleLocation = (await locationPath?.getData?.(new zip.TextWriter()))
+        var locations: Location[] = [];
+        if (possibleLocation) {
+            locations = Deserialize.deserializeLocationRecords(possibleLocation);
+        }
+        
+        let routes = new Map<string, Map<string, Route[]>>();
+
+        const routePaths = entries.filter((entry) => entry.filename.startsWith("Takeout/Location History/Semantic Location History/"));
+    
+        for (const routePath of routePaths) {
+            const possibleRoute = (await routePath?.getData?.(new zip.TextWriter()));
+            if (possibleRoute) {
+                const route = Deserialize.deserializeRouteRecords(possibleRoute);
+                const date = routePath.filename.split("/")[4].split("_");
+                const year = date[0];
+                const month = date[1];
+                if (!routes.has(year)) {
+                    routes.set(year, new Map<string, Route[]>());
+                }
+                if (!routes.get(year)?.has(month)) {
+                    routes.get(year)?.set(month, []);
+                }
+                routes.get(year)?.get(month)?.push(...route);
+            }
+        }
+    }
+}
+
