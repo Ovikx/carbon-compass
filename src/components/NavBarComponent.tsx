c8e5ab5a846a0992fc105ccc04078c178410074b
Ovikx <ovikdas10@gmail.com>
2023-09-23 14:36:57 -0500

    change nav styles

--- src/components/NavBarComponent.tsx
+++ src/components/NavBarComponent.tsx
@@ -1,14 +1,23 @@
-import { NavLink } from "react-router-dom";
-
 export function NavBarComponent() {
-    return (
-        <header className="top-0 left-0 px-30 py-100 flex flex-row sticky mb-10 justify-between align-middle shadow-sm ">
-            <h2 className="text-2xl text-emerald-500 cursor-pointer font-extrabold">Carbon Tracker</h2>
-            <nav>
-                <a href="#" className="text-lg no-underline text-emerald-500 px-3 py-15 rounded-md mx-0 my-10 font-bold hover:bg-emerald-500 hover:rounded-lg hover:text-white">Home</a>
-                <a href="#" className="text-lg no-underline text-emerald-500 px-3 py-15 rounded-md mx-0 my-10 font-bold hover:bg-emerald-500 hover:rounded-lg hover:text-white">Leaderboard</a>
-                <a href="#" className="text-lg no-underline text-emerald-500 px-3 py-15 rounded-md mx-0 my-10 font-bold hover:bg-emerald-500 hover:rounded-lg hover:text-white">Tracker</a>
-            </nav>
-        </header>
-    )
+  const buttonStyle =
+    "text-lg no-underline text-emerald-500 px-3 py-2 rounded-md mx-2 my-10 font-bold hover:bg-emerald-500 hover:rounded-lg hover:text-white transition-all duration-200";
+
+  return (
+    <header className="top-0 left-0 px-30 pb-5 flex flex-row sticky mb-10 justify-between align-middle shadow-sm ">
+      <h2 className="text-2xl text-emerald-500 cursor-pointer font-extrabold">
+        Carbon Tracker
+      </h2>
+      <nav className="gap-2">
+        <a href="#" className={buttonStyle}>
+          Home
+        </a>
+        <a href="#" className={buttonStyle}>
+          Leaderboard
+        </a>
+        <a href="#" className={buttonStyle}>
+          Tracker
+        </a>
+      </nav>
+    </header>
+  );
 }
