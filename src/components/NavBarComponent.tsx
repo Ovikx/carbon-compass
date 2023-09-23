55b0b4402bff1f2b8082c1c747e08ffe4a1add5c
James Loh <lohjames16@gmail.com>
2023-09-23 11:27:28 -0500

    test

---
+++ src/components/NavBarComponent.tsx
@@ -0,0 +1,12 @@
+export function NavBarComponent() {
+    return (
+        <header>
+            <h2 >Carbon Tracker</h2>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Leaderboard</a>
+                <a href="#">Tracker</a>
+            </nav>
+        </header>
+    )
+}
