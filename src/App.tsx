26e8746fde5dcb56cca01d60ab994fa17431e629
Ovikx <ovikdas10@gmail.com>
2023-09-23 14:01:38 -0500

    stop yelling at me eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { NavBarComponent } from './components/NavBarComponent'
-import { Home } from './pages/Home'
-import { Routes, Route } from "react-router-dom"
-import { Tracker } from './pages/Tracker'
-import { Leaderboard } from './pages/Leaderboard'
+import "./App.css";
+import { NavBarComponent } from "./components/NavBarComponent";
+import { Home } from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { Tracker } from "./pages/Tracker";
+import { Leaderboard } from "./pages/Leaderboard";
 
 function App() {
-
-
   return (
     <>
       <div className="bg-white/80">
-        <NavBarComponent/>
+        <NavBarComponent />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/tracker" element={<Tracker />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
-
         </Routes>
       </div>
-      
     </>
   );
 }
