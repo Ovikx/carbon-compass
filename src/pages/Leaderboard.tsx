a01c245298d7dda312da7bd43db98c298e6ec126
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2023-09-23 16:40:18 -0500

    Make a dummy table

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -1,13 +1,32 @@
 export function Leaderboard() {
-    return (
-        <div className="flex flex-col">
-            <div>
-            <h1 className='left-align text-lg'>Leaderboard</h1>
-            <p className='left-align'>Top 10</p>
-            <p className='left-align'>All</p>
-            
+  // leaderboard page for users to see how they compare to others
 
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col">
+      <div>
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Name</th>
+              <th className="px-4 py-2">Carbon Offset</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border px-4 py-2">John</td>
+              <td className="border px-4 py-2">10</td>
+            </tr>
+            <tr className="bg-gray-100">
+              <td className="border px-4 py-2">Jane</td>
+              <td className="border px-4 py-2">5</td>
+            </tr>
+            <tr>
+              <td className="border px-4 py-2">Bob</td>
+              <td className="border px-4 py-2">7</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
