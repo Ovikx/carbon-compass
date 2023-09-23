855a3cfc0d9abebeb8505cf67979608479e5cb33
Ovikx <ovikdas10@gmail.com>
2023-09-23 16:50:15 -0500

    use file context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { FileUploader } from "../components/FIleUploader";
+
 export function Home() {
   return (
     <>
@@ -5,11 +7,17 @@ export function Home() {
         <div className="flex flex-col absolute left-10 pl-7">
           <p className="text-2xl">Track your Carbon Footprint,</p>
           <p className="text-2xl pr-[220px]">Smarter</p>
-        </div>  
+        </div>
       </div>
 
-        <input type = "file" id = "locationData" className="hidden" onChange={} name="userData"/>
-        <button className="flex transition-all flex-row ml-72 mt-28 text-lg rounded-lg p-1 text-white font-semibold hover:bg-green-700 bg-green-600 ">
+      <input
+        type="file"
+        id="locationData"
+        className="hidden"
+        onChange={() => {}}
+        name="userData"
+      />
+      <button className="flex transition-all flex-row ml-72 mt-28 text-lg rounded-lg p-1 text-white font-semibold hover:bg-green-700 bg-green-600 ">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="rounded-lg pt-1 hover:bg-green-700 fill-white"
@@ -21,6 +29,7 @@ export function Home() {
         </svg>
         <p className="pt-1 pb-1 pr-3">Upload!</p>
       </button>
+      <FileUploader />
     </>
   );
 }
