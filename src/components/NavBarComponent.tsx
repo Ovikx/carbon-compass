9a58d54b0348712e08a429ae3b0ca14bca66535b
Ovikx <ovikdas10@gmail.com>
2023-09-24 08:47:40 -0500

    fix nav bar spacing

--- src/components/NavBarComponent.tsx
+++ src/components/NavBarComponent.tsx
@@ -4,11 +4,11 @@ export function NavBarComponent() {
   return (
     <nav className="fixed top-0 left-0 w-full ">
       <div className="container mx-auto px-4 py-2">
-        <div className="flex justify-between items-center mt-5">
+        <div className="flex justify-between items-center mt-5 px-10">
           <h2 className="text-2xl text-emerald-400 cursor-pointer font-extrabold">
             Carbon Compass
           </h2>
-          <ul className="flex space-x-4">
+          <ul className="flex gap-10 space-x-4">
             <li>
               <NavLink
                 to="/"
